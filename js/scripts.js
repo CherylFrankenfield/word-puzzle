c3462ad914a4bf27411bf6cf449539725551d44e
Cheryl Frankenfield and Sam Kissee <cheryl@>
2017-10-24 10:09:34 -0700

    Add text to puzzle. add variables

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  var str = "Hello Person"
+  var vowels = ["a", "e", "i", "o", "u"]
+  var answer = []
+  split.forEach(function(vowel) {
+   $(vowels).replace("-");
+    answer.push(split)
+    alert(answer);
+})
+
+// var seperate = str.split("");
+// var split = split.replace(vowels, "-")
+
+
+});
