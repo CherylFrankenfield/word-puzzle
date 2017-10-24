cfe635c6536a4d7a2468f3361e432ea8d06d48fd
Cheryl Frankenfield and Sam Kissee <cheryl@>
2017-10-24 11:55:10 -0700

    Add for loop and if else statement to problem

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,53 @@
-$(document).ready(function() {
-  $("form#form1").submit(function(event){
-  event.preventDefault();
-});
-  var sentence = $("p#sentence").val();
-  alert(sentence);
-  var vowels = ["a", "e", "i", "o", "u"]
-  var answer = []
-  split.forEach(function(vowel) {
-   $(vowels).replace("-");
-    answer.push(split)
-    alert(answer);
-});
-
-// var seperate = str.split("");
-// var split = split.replace(vowels, "-")
-
-
-});
-var sentence1 = "Testing"
-var vowels = ["a", "e", "i", "o", "u"]
-var seperated = sentence1.split("");
+// $(document).ready(function() {
+//   $("form#form1").submit(function(event){
+//   event.preventDefault();
+// });
+//   var vowels = ["a", "e", "i", "o", "u"]
+//   var sentence = $("p#sentence").val();
+//   alert(sentence);
+//   var answer = []
+//   split.forEach(function(vowel) {
+//    $(vowels).replace("-");
+//     answer.push(split)
+//     alert(answer);
+// });
+//
+// // var seperate = str.split("");
+// // var split = split.replace(vowels, "-")
+//
+//
+// });
+// var sentence1 = "Testing"
+// var vowels = ["a", "e", "i", "o", "u"];
+// var seperates = [sentence1.split("")];
+// var answer = []
+// var newSeperates = seperates.map(function(vowel) {
+//   return (vowels).replace("-");
+//   answer.push(vowel);
+// });
+// seperates.forEach(function(seperate){
+//   (vowels === "-"),
+//   answer.push(vowels + seperate);
+// });
+// strr.split('a').join(' ').split('e').join(' ').split('i').join('').split('o').join(' ').split('u').join(' ')
+// seperated.forEach(function(vowel) {
+//   seperated.split(vowels).join('-');
+//
+// })
+// (vowels === "-")
+//
+// if ("" === vowels){
+//
+// }
 var answer = []
-seperated.forEach(function(seperate){
-  vowels.forEach(function(vowel){
-    $(vowels).append("-");
-    answer.push(vowel + seperate);
-  });
-});
-strr.split('a').join(' ').split('e').join(' ').split('i').join('').split('o').join(' ').split('u').join(' ')
-seperated.forEach(function(vowel) {
-  seperated.split(vowels).join('-');
+var vowels = ["a", "e", "i", "o", "u"];
+var sentence1 = "Testing"
+var seperates = [sentence1.split("")];
+for (var index = 0; index< seperates.length; index+= 1){
+  if(index === vowel){
+    $()return answer += "-")
+  } else {
+    $(return answer += index);
+  } alert(answer);
 
-})
+};
